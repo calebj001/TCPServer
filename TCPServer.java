fc1b2681df6736fe281eddc3526c454231f2d184
calebjohnson <001calebjohn001@gmail.com>
2023-11-15 19:41:26 -0600

    Client and Server Modified, Client mostly complete

--- TCPServer.java
+++ TCPServer.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class myFirstTCPServer {
+public class TCPServer {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java myFirstTCPServer <port>");
